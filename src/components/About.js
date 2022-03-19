8cbb881f268018a9148131c9315e42ec421b11a8
Raj Shankar Tiwary <rajshankartiwary@gmail.com>
2022-03-19 22:03:51 +0530

    links open in new tab

--- src/components/About.js
+++ src/components/About.js
@@ -21,8 +21,8 @@ const About = () => {
                 I'm a CSE undergrad in my final year and I'm looking for <span className='underline decoration-purple-600 decoration-4'>frontend internships</span> where I can contribute and learn, and also join as a full timer if given a chance to create an even bigger impact.
             </p>
             <br></br>
-            <p className='text-sm'>Hit me up on <a href='https://twitter.com/ravoluzen' className='underline decoration-green-500 decoration-2 hover:decoration-purple-600'>Twitter</a> or <a href='https://www.linkedin.com/in/raj-shankar-tiwary-053b86189/' className='underline decoration-green-500 decoration-2 hover:decoration-purple-600'>Linkedin</a>.</p>
-            <p className='text-sm'>Or send a <a href='mailto: rajshankartiwary@gmail.com' className='underline decoration-green-500 decoration-2 hover:decoration-purple-600'>mail</a>.</p>
+            <p className='text-sm'>Hit me up on <a href='https://twitter.com/ravoluzen' taregt='_blank.' className='underline decoration-green-500 decoration-2 hover:decoration-purple-600'>Twitter</a> or <a href='https://www.linkedin.com/in/raj-shankar-tiwary-053b86189/' taregt='_blank.' className='underline decoration-green-500 decoration-2 hover:decoration-purple-600'>Linkedin</a>.</p>
+            <p className='text-sm'>Or send a <a href='mailto: rajshankartiwary@gmail.com' taregt='_blank.' className='underline decoration-green-500 decoration-2 hover:decoration-purple-600'>mail</a>.</p>
             <br></br>
             <p className='text-sm'>Thanks for reading :)</p>
         </article>
