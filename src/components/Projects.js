3c6862d20e56324ed25a78b686d07a32a06703f4
Raj Shankar Tiwary <rajshankartiwary@gmail.com>
2022-11-16 12:41:41 +0530

    new projects addition

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,6 +4,9 @@ import ye from '../images/ye.png'
 import witw from '../images/witw.png'
 import ip from '../images/ip.png'
 import hd from '../images/hd.png'
+import candid from '../images/candid.png'
+import movierata from '../images/movierata.png'
+import musix from '../images/musix.png'
 
 
 const Projects = () => {
@@ -15,6 +18,33 @@ const Projects = () => {
     transition={{ delay: 0.3 }}>
         <h1 className='font-bold text-base mb-8'>Some of my recent projects</h1>
         <main className='w-full grid grid-cols-1 gap-10 items-start md:grid-cols-2 lg:grid-cols-4'>
+            <article>
+                <img src={musix} alt='project thumbnail' className='object-cover'></img>
+                <h3 className='font-bold underline decoration-purple-600 decoration-4 mb-2'>Musix</h3>
+                <p className='text-sm mb-4'>A full-fledged music streaming and exploration app, powered by Shazam Core API.</p>
+                <span>
+                    <a href='https://ravoluzen-musix.netlify.app' target='_blank.' className='mt-12 underline decoration-green-400'>live site</a>
+                    <a href='https://github.com/ravoluzen/musix' target='_blank.' className='m-12 underline decoration-green-400'>source code</a>
+                </span>
+            </article>
+            <article>
+                <img src={candid} alt='project thumbnail' className='object-cover'></img>
+                <h3 className='font-bold underline decoration-purple-600 decoration-4 mb-2'>Candid</h3>
+                <p className='text-sm mb-4'>A platform to post your candid thoughts and interact with others' candids.</p>
+                <span>
+                    <a href='https://candid.vercel.app' target='_blank.' className='mt-12 underline decoration-green-400'>live site</a>
+                    <a href='https://github.com/ravoluzen/candid' target='_blank.' className='m-12 underline decoration-green-400'>source code</a>
+                </span>
+            </article>
+            <article>
+                <img src={movierata} alt='project thumbnail' className='object-cover'></img>
+                <h3 className='font-bold underline decoration-purple-600 decoration-4 mb-2'>Movierata</h3>
+                <p className='text-sm mb-4'>Web app that uses OMDB API to display movies and shows along with a lot of info like plot, ratings, awards, etc. Also lets the user search movies and shows and view anyone of them in detail.</p>
+                <span>
+                    <a href='https://movierata.vercel.app/' target='_blank.' className='mt-12 underline decoration-green-400'>live site</a>
+                    <a href='https://github.com/ravoluzen/movierata' target='_blank.' className='m-12 underline decoration-green-400'>source code</a>
+                </span>
+            </article>
             <article className=''>
                 <img src={witw} alt='project thumbnail' className='object-cover'></img>
                 <h3 className='font-bold underline decoration-purple-600 decoration-4 mb-2'>Where in the world?</h3>
