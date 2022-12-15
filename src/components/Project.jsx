7a67807964edef18403f1c3426b1c287da58f63b
Raj Shankar Tiwary <rajshankartiwary@gmail.com>
2022-12-15 15:39:52 +0530

    project thumbnail resize, add shadow to project tile

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,10 +5,10 @@ const Project = ({ image, title, description, site, github }) => {
   
   return (
     <div>
-        <article className='border-2 border-purple-800 rounded-md p-4 h-96 flex flex-col justify-between'>
+        <article className='border-2 shadow-md shadow-slate-800 border-purple-800 rounded-md p-4 h-96 flex flex-col justify-between'>
             <div>
-                <div className='border-2 border-gray-400 mb-4 rounded-md'>
-                    <img src={image} alt='project thumbnail' className='basis-1/4 object-cover'></img>
+                <div className='border-2 border-gray-400 mb-4 rounded-md h-36 object-fill'>
+                    <img src={image} alt='project thumbnail' className='object-cover h-full w-full'></img>
                 </div>
                 <h3 className='font-bold underline decoration-purple-600 decoration-4 mb-2'>{title}</h3>
                 <p className='text-sm mb-4'>{description}</p>
