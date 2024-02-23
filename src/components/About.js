e080191ee2cf2bf62945fb9c42141846a382f272
Raj <rajshankartiwary@gmail.com>
2024-02-23 13:11:04 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -18,14 +18,14 @@ const About = () => {
                 Oh also, I'm a huge fan of EDM, Rock and Hip-Hop music!
             </p>
             <br></br>
-            <p className='text-sm'>
-                I'm a CSE undergrad('22) and I'm looking for <span className='underline decoration-purple-600 decoration-4'>full time frontend developer roles.</span>
-            </p>
-            <br></br>
-            <p className='text-sm'>Hit me up on <a href='https://twitter.com/ravoluzen' target='_blank.' className='underline decoration-green-500 decoration-2 hover:decoration-purple-600'>Twitter</a> or <a href='https://www.linkedin.com/in/raj-shankar-tiwary-053b86189/' target='_blank.' className='underline decoration-green-500 decoration-2 hover:decoration-purple-600'>Linkedin</a>.</p>
-            <p className='text-sm'>Or send a <a href='mailto: rajshankartiwary@gmail.com' target='_blank.' className='underline decoration-green-500 decoration-2 hover:decoration-purple-600'>mail</a>.</p>
-            <br></br>
-            <p className='text-sm'>Thanks for reading :)</p>
+            // <p className='text-sm'>
+            //     I'm a CSE undergrad('22) and I'm looking for <span className='underline decoration-purple-600 decoration-4'>full time frontend developer roles.</span>
+            // </p>
+            // <br></br>
+            // <p className='text-sm'>Hit me up on <a href='https://twitter.com/ravoluzen' target='_blank.' className='underline decoration-green-500 decoration-2 hover:decoration-purple-600'>Twitter</a> or <a href='https://www.linkedin.com/in/raj-shankar-tiwary-053b86189/' target='_blank.' className='underline decoration-green-500 decoration-2 hover:decoration-purple-600'>Linkedin</a>.</p>
+            // <p className='text-sm'>Or send a <a href='mailto: rajshankartiwary@gmail.com' target='_blank.' className='underline decoration-green-500 decoration-2 hover:decoration-purple-600'>mail</a>.</p>
+            // <br></br>
+            <p className='text-sm'>When I'm not making web interfaces, I can be found creating music or writing rap verses, or reading about dinosaurs, watching anime or learning how to play keys. :)</p>
         </article>
     </motion.div>
   )
